c554ff740fcd6fa2d889aea9a7596f5f30401eae
lanceolata <lanceolatayuan@gmail.com>
2020-11-19 23:50:21 +0800

    update

--- algorithms/algorithms-java/src/main/java/com/lanceolata/coding/algorithms/java/leetcode/Question_0002_Add_Two_Numbers.java
+++ algorithms/algorithms-java/src/main/java/com/lanceolata/coding/algorithms/java/leetcode/Question_0002_Add_Two_Numbers.java
@@ -5,10 +5,9 @@ public class Question_0002_Add_Two_Numbers {
     public class ListNode {
         int val;
         ListNode next;
-        ListNode(int x) {
-            val = x;
-            next = null;
-        }
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
     }
 
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
