920a76e24b5002ec522c484b98170c4175afaf7d
lanceolata <lanceolatayuan@gmail.com>
2020-12-14 01:03:13 +0800

    update

--- algorithms/algorithms-java/src/main/java/com/lanceolata/coding/algorithms/java/leetcode/Question_0082_Remove_Duplicates_from_Sorted_List_II.java
+++ algorithms/algorithms-java/src/main/java/com/lanceolata/coding/algorithms/java/leetcode/Question_0082_Remove_Duplicates_from_Sorted_List_II.java
@@ -5,10 +5,9 @@ public class Question_0082_Remove_Duplicates_from_Sorted_List_II {
     public class ListNode {
         int val;
         ListNode next;
-        ListNode(int x) {
-            val = x;
-            next = null;
-        }
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
     }
 
     public ListNode deleteDuplicates(ListNode head) {
--- algorithms/algorithms-java/src/main/java/com/lanceolata/coding/algorithms/java/leetcode/Question_0083_Remove_Duplicates_from_Sorted_List.java
+++ algorithms/algorithms-java/src/main/java/com/lanceolata/coding/algorithms/java/leetcode/Question_0083_Remove_Duplicates_from_Sorted_List.java
@@ -5,10 +5,9 @@ public class Question_0083_Remove_Duplicates_from_Sorted_List {
     public class ListNode {
         int val;
         ListNode next;
-        ListNode(int x) {
-            val = x;
-            next = null;
-        }
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
     }
 
     public ListNode deleteDuplicates(ListNode head) {
