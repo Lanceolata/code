0f92a24a6ee4b890df051299f69911b58aa6a71e
lanceolata <lanceolatayuan@gmail.com>
2021-06-25 13:10:54 +0800

    update

--- algorithms/algorithms-cplusplus/leetcode/Question_0454_4Sum_II.cc
+++ algorithms/algorithms-cplusplus/leetcode/Question_0454_4Sum_II.cc
@@ -5,16 +5,16 @@ using namespace std;
 
 class Solution {
  public:
-  int fourSumCount(vector<int>& A, vector<int>& B, vector<int>& C, vector<int>& D) {
+  int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) {
     int res = 0;
     unordered_map<int, int> m;
-    for(auto i : A){
-      for(auto j : B){
+    for(auto i : nums1){
+      for(auto j : nums2){
         m[i + j]++;
       }
     }        
-    for(auto i : C){
-      for(auto j : D){
+    for(auto i : nums3){
+      for(auto j : nums4){
         if(m.count(-(i + j))){
           res += m[-(i + j)];
         }
--- algorithms/algorithms-java/src/main/java/com/lanceolata/coding/algorithms/java/leetcode/Question_0454_4Sum_II.java
+++ algorithms/algorithms-java/src/main/java/com/lanceolata/coding/algorithms/java/leetcode/Question_0454_4Sum_II.java
@@ -4,10 +4,10 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Question_0454_4Sum_II {
-    public int fourSumCount(int[] A, int[] B, int[] C, int[] D) {
+    public int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
         Map<Integer, Integer> map = new HashMap<>();
-        for (int i : A) {
-            for (int j : B) {
+        for (int i : nums1) {
+            for (int j : nums2) {
                 if (map.containsKey(i + j)) {
                     map.put(i + j, map.get(i + j) + 1);
                 } else {
@@ -16,8 +16,8 @@ public class Question_0454_4Sum_II {
             }
         }
         int res = 0;
-        for (int i : C) {
-            for (int j : D) {
+        for (int i : nums3) {
+            for (int j : nums4) {
                 if (map.containsKey(-(i + j))) {
                     res += map.get(-(i + j));
                 }
