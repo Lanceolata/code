570602d3a9a8518b8df18126872a4fd8c78f9258
lanceolata <lanceolatayuan@gmail.com>
2022-07-20 12:45:47 +0800

    update

--- algorithms/algorithms-java/src/main/java/com/lanceolata/coding/algorithms/java/leetcode/Question_0377_Combination_Sum_IV.java
+++ algorithms/algorithms-java/src/main/java/com/lanceolata/coding/algorithms/java/leetcode/Question_0377_Combination_Sum_IV.java
@@ -5,22 +5,16 @@ import java.util.Arrays;
 public class Question_0377_Combination_Sum_IV {
     public int combinationSum4(int[] nums, int target) {
         int[] dp = new int[target + 1];
-        Arrays.fill(dp, -1);
         dp[0] = 1;
-        return help(nums, target, dp);
-    }
-
-    private int help(int[] nums, int target, int[] dp) {
-        if (dp[target] != -1) {
-            return dp[target];
-        }
-        int res = 0;
-        for (int i : nums) {
-            if (target >= i) {
-                res += help(nums, target - i, dp);
+        Arrays.sort(nums);
+        for (int i = 1; i <= target; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if (nums[j] > i) {
+                    break;
+                }
+                dp[i] += dp[i - nums[j]];
             }
         }
-        dp[target] = res;
-        return res;
+        return dp[target];
     }
 }
