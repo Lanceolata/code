47092ba5b896569b55f1456f6c1cce385a0becde
lanceolata <lanceolatayuan@gmail.com>
2022-10-14 09:11:38 +0800

    update

--- algorithms/algorithms-cplusplus/leetcode/Question_0220_Contains_Duplicate_III.cc
+++ algorithms/algorithms-cplusplus/leetcode/Question_0220_Contains_Duplicate_III.cc
@@ -6,38 +6,38 @@ using namespace std;
 
 class Solution {
  public:
-  bool containsNearbyAlmostDuplicate(vector<int>& nums, int k, int t) {
+  bool containsNearbyAlmostDuplicate(vector<int>& nums, int indexDiff, int valueDiff) {
     multiset<long long> window;
     for (int i = 0; i < nums.size(); ++i) {
-      if (i > k) window.erase(nums[i - k - 1]);
-      auto it = window.lower_bound((long long) nums[i] - t);
-      if (it != window.end() && (long long) *it - nums[i] <= t) return true;
+      if (i > indexDiff) window.erase(nums[i - indexDiff - 1]);
+      auto it = window.lower_bound((long long) nums[i] - valueDiff);
+      if (it != window.end() && (long long) *it - nums[i] <= valueDiff) return true;
       window.insert(nums[i]);
     }
     return false;
   }
 
-  bool containsNearbyAlmostDuplicate2(vector<int>& nums, int k, int t) {
+  bool containsNearbyAlmostDuplicate2(vector<int>& nums, int indexDiff, int valueDiff) {
     unordered_set<long> s;
-    if (k >= nums.size()) {
-      k = nums.size() - 1;
+    if (indexDiff >= nums.size()) {
+      indexDiff = nums.size() - 1;
     }
     for(int i = 0; i < nums.size(); i ++) {
-      if (t < k) {
-        for(int j = nums[i] - t; j <= nums[i] + t; j ++ ) {
+      if (valueDiff < indexDiff) {
+        for(int j = nums[i] - valueDiff; j <= nums[i] + valueDiff; j ++ ) {
           if (s.count(j)) return true;
         }
       } else {
         for(auto it = s.begin(); it != s.end(); it++) {
-          if (abs(*it - nums[i]) <= t) {
+          if (abs(*it - nums[i]) <= valueDiff) {
             return true;
           }
         }
       }
             
       s.insert(nums[i]);
-      if (s.size() > k) {
-        s.erase(nums[i - k]);
+      if (s.size() > indexDiff) {
+        s.erase(nums[i - indexDiff]);
       }
     }
     return false;
--- algorithms/algorithms-java/src/main/java/com/lanceolata/coding/algorithms/java/leetcode/Question_0220_Contains_Duplicate_III.java
+++ algorithms/algorithms-java/src/main/java/com/lanceolata/coding/algorithms/java/leetcode/Question_0220_Contains_Duplicate_III.java
@@ -3,18 +3,18 @@ package com.lanceolata.coding.algorithms.java.leetcode;
 import java.util.TreeSet;
 
 public class Question_0220_Contains_Duplicate_III {
-    public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
-        if (nums == null || nums.length <= 1 || k < 1 || t < 0) {
+    public boolean containsNearbyAlmostDuplicate(int[] nums, int indexDiff, int valueDiff) {
+        if (nums == null || nums.length <= 1 || indexDiff < 1 || valueDiff < 0) {
             return false;
         }
         TreeSet<Long> set = new TreeSet<>();
         for (int i = 0; i < nums.length; i++) {
-            if (i > k) {
-                set.remove((long)nums[i - k - 1]);
+            if (i > indexDiff) {
+                set.remove((long) nums[i - indexDiff - 1]);
             }
             long n = nums[i];
-            if ((set.floor(n) != null && n <= t + set.floor(n))
-                    || (set.ceiling(n) != null && set.ceiling(n) <= t + n)) {
+            if ((set.floor(n) != null && n <= valueDiff + set.floor(n))
+                    || (set.ceiling(n) != null && set.ceiling(n) <= valueDiff + n)) {
                 return true;
             }
             set.add(n);
