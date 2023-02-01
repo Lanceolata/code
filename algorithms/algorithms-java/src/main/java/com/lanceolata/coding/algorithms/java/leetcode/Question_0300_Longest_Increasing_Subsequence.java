8469b682b29058eb0e652d0159e5835040fdc46e
lanceolata <lanceolatayuan@gmail.com>
2023-02-01 09:49:33 +0800

    ipdate

--- algorithms/algorithms-java/src/main/java/com/lanceolata/coding/algorithms/java/leetcode/Question_0300_Longest_Increasing_Subsequence.java
+++ algorithms/algorithms-java/src/main/java/com/lanceolata/coding/algorithms/java/leetcode/Question_0300_Longest_Increasing_Subsequence.java
@@ -4,6 +4,22 @@ import java.util.Arrays;
 
 public class Question_0300_Longest_Increasing_Subsequence {
     public int lengthOfLIS(int[] nums) {
+        int res = 0, cur = 0;
+        int[] dp = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            cur = 0;
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    cur = Math.max(cur, dp[j]);
+                }
+            }
+            dp[i] = cur + 1;
+            res = Math.max(res, dp[i]);
+        }
+        return res;
+    }
+
+    public int lengthOfLISOpt(int[] nums) {
         int[] dp = new int[nums.length];
         int len = 0;
         for (int num : nums) {
