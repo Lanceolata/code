4be5614691a8b3034e6080e7804d3768c7d169f9
lanceolata <lanceolatayuan@gmail.com>
2023-10-08 09:35:07 +0800

    update

---
+++ algorithms/algorithms-cplusplus/leetcode/Question_1038_Binary_Search_Tree_to_Greater_Sum_Tree.cc
@@ -0,0 +1,32 @@
+
+// Definition for a binary tree node.
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+ public:
+  TreeNode* bstToGst(TreeNode* root) {
+    if (!root) {
+      return root;
+    }
+    int sum = 0;
+    help(root, sum);
+    return root;
+  }
+
+  void help(TreeNode* root, int& sum) {
+    if (!root) {
+      return;
+    }
+    help(root->right, sum);
+    sum += root->val;
+    root->val = sum;
+    help(root->left, sum);
+  }
+};
